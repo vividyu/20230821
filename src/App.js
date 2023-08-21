c2a663e6e652a28613055c1c41a0e7383bcd70cd
Guanyu Hou <10951700+vividyu@users.noreply.github.com>
2023-08-21 14:25:42 -0400

    remove strict mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+
+const ListItem = React.memo(({ value, increment }) => {
+  const renders = useRef(0);
+  renders.current++;
+
+  return (
+    <li>
+      {value} - Rendered {renders.current} times
+      <button onClick={increment}>Increment</button>
+    </li>
+  );
+});
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([{ id: 1, value: "Item 1" }]);
+
+  useEffect(() => {
+    const newItems = items.map((item) => {
+      if (item.id === 1) {
+        return { ...item, value: `Item 1 (clicked ${count} times)` };
+      }
+      return item;
+    });
+
+    setItems(newItems);
+  }, [count]);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter App</h1>
+      <p>You've clicked {count} times.</p>
+      <ul>
+        {items.map((item) => (
+          <ListItem
+            key={item.id}
+            value={item.value}
+            increment={incrementCount}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
